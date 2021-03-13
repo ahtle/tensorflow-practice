71e31bf891d15f83be1c822dceeaf7d745f6fe4c
Anh Le <anh.ht.le@gmail.com>
2021-03-13 03:03:08 -0800

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import './styles/index.css';
 function App() {
   return (
     <>
-      <Router>  
+      <Router basename={process.env.PUBLIC_URL}>  
         <MainHeader />
 
         <div className="min-h-screen">
--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -14,7 +14,7 @@ const ProjectCard = (props: Props) => {
             to={props.path}
         >
             <div className="img-wrapper flex items-center border-b border-gray-200 bg-blue-50">
-                <img src={`/images/${props.image}`} alt={props.name}/>
+                <img src={`${process.env.PUBLIC_URL}/images/${props.image}`} alt={props.name}/>
             </div>
             <div className='card-body p-4 group-hover:bg-yellow-600 transition'>
                 <p className="text-bold text-blue-900 text-lg mb-2 group-hover:text-white">{props.name}</p>
